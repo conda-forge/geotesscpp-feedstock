1336fa29fdfc62151c0386623b50cba3d6e77f2f
jkmacc-LANL <jkmacc@lanl.gov>
2024-07-15 12:22:11 -0600

    Relocatable (#2)
    * Include AK135Model.h in release
    
    * MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.36.2, and conda-forge-pinning 2024.07.11.08.43.30
    
    * Add build recipe
    
    * Fix AK135 path
    
    * Pass Conda-Build flags to Makefile
    
    Inject conda-build compiler flags into a variable used in the Makefile by
    overwriting it, including some of what it included originally.
    Hopefully, this will pass @rpath and make the library relocatable.
    
    * Increment build number
    
    * Improve comment
    
    * Debug compiler flags
    
    * Make literal debug statement
    
    * Remove potentially problematic flags
    
    * Add quotes to group flags
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,26 @@
 
 set -e
 
-make all CC=${CC}
+# Put conda-build compiler flags into a variable already used in the
+# Makefile by overriding it, including some of what it included originally.
+# Hopefully, this will pass conda-build's @rpath and make the library
+# relocatable.
+
+# CCFLAGS="${CFLAGS} ${CXXFLAGS} ${LDFLAGS} -m64 -O3"
+# CCFLAGS="${CXXFLAGS} ${LDFLAGS} -m64 -O3"
+CCFLAGS="${CXXFLAGS} ${LDFLAGS}"
+
+# if [[ `uname` == 'Linux' ]]; then
+#     # g++
+#     CCFLAGS="${CCFLAGS} -DLinux"
+# else
+#     # Apple Clang
+#     CCFLAGS="${CCFLAGS} -DDarwin"
+# fi
+
+echo "make all CC=${CC} CCFLAGS=${CCFLAGS}"
+make all CC=${CC} CCFLAGS="${CCFLAGS}"
+# make all CC=${CC} CCFLAGS=${CCFLAGS} LIB=${PREFIX}/lib
 
 # copy dynamic libraries into standard location
 cp lib/* $PREFIX/lib/
